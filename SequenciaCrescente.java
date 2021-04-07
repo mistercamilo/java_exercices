734c2aca3be77f095df556ce9d03e523c0f40b77
Camilo Vieira <camilo.vieira@gmail.com>
2021-04-06 21:30:40 -0300

    Added condition to search values inside arraylist and check the ascendent order

--- SequenciaCrescente.java
+++ SequenciaCrescente.java
@@ -1,37 +1,36 @@
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
-import java.util.Collections;
 
-public class SequenciaCrescente{
+public class SequenciaCrescente {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         int i = 0;
         int input;
+        boolean isCrescente = true;
         ArrayList<Integer> array = new ArrayList<Integer>();
 
-        ArrayList<Integer> arraySorted = new ArrayList<Integer>();
-
-        do{
+        do {
             input = Integer.parseInt(JOptionPane.showInputDialog("Valor :"));
             array.add(input);
             i++;
-        }
-        while (input != 0);
-        
-        System.out.println("Array normal:");
-        System.out.println(array);
-        
-        System.out.println("Array ordenado:");
-        Collections.sort(array);
-        System.out.println(array);
+        } while (input != 0);
 
-        boolean isEqual = array.equals(arraySorted);
-
-        if (isEqual == true){
-            System.out.println("Está em ordem crescente.");
-        }else{
-            System.out.println("Não está em ordem crescente.");
+        System.out.println("Tamanho do array :" + array.size());
+        for (int a = 0; a < (array.size() - 2); a++) {
+            if (array.get(a) > array.get(a + 1)) {
+                // System.out.println(array.get(a) + " > " + array.get(a + 1));
+                isCrescente = false;
+                break;
+            } else {
+                // System.out.println(array.get(a) + " < " + array.get(a + 1));
+                isCrescente = true;
+            }
         }
-     }
+        if (isCrescente) {
+            System.out.println(array + "Sim");
+        } else {
+            System.out.println(array + "Nao");
+        }
+    }
 }
